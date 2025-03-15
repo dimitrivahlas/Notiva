57bea9295d546eb7aef8bea85ceb47252fc29564
dimitrivahlas <vahlasdl@gmail.com>
2025-03-15 14:31:03 -0700

    Record button

---
+++ frontend/src/components/RecordButton.tsx
@@ -0,0 +1,12 @@
+import { startSpeechRecognition } from "../utils/speechToText";
+
+export default function RecordButton({ onTranscript }: { onTranscript: (text: string) => void }) {
+  return (
+    <button
+      onClick={() => startSpeechRecognition(onTranscript)}
+      className="bg-red-500 text-white px-4 py-2 rounded mt-2"
+    >
+      🎤 Record Voice
+    </button>
+  );
+}
